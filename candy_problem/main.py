c536cb700fcbcb72eb72fe9e1eddc003b7ea87c7
karishma <karishmacarterjohnson@gmail.com>
2021-09-14 10:18:54 -0700

    passes test cases, define create_new_candy_data_structure()

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -25,8 +25,24 @@ the data structure made in `create_new_candy_data_structure()`.
 
 
 def create_new_candy_data_structure(data):
-    pass 
+    candy_favoriters = {}
+    for friend in data:
+        for candy in friend[1]:
+            if candy in candy_favoriters:
+                candy_favoriters[candy].append(friend[0])
+            else:
+                candy_favoriters[candy] = [friend[0]]
+    print("candies!", candy_favoriters)
+    return candy_favoriters
 
+friend_favorites = [
+    [ "Sally", [ "lollipop", "bubble gum", "laffy taffy" ]],
+    [ "Bob", [ "milky way", "licorice", "lollipop" ]],
+	[ "Arlene", [ "chocolate bar", "milky way", "laffy taffy" ]],
+	[ "Carlie", [ "nerds", "sour patch kids", "laffy taffy" ]]
+]
+
+create_new_candy_data_structure(friend_favorites)
 
 def get_friends_who_like_specific_candy(data, candy_name):
     pass
